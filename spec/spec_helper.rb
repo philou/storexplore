c683ea499c7b27933ce229b0327fcbe175ef4889
Philou <philippe.bourgau@free.fr>
2014-08-21 07:16:33 +0200

    Integrate code climate coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301  USA
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 require 'fakeweb'
 require 'spec_combos'
 require 'storexplore'
